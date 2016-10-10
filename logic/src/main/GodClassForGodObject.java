9ad59d7d86cf944fb9237da25bd94007a82d6cd0
Oleksandr Kozin <Tryhanuch@users.noreply.github.com>
2016-10-10 16:21:15 +0300

    fixed!
    It would be better.

---
+++ logic/src/main/GodClassForGodObject.java
@@ -0,0 +1,11 @@
+public class GodClassForGodObject {
+    public static void main(String[] args) {
+        System.out.println(doZbs());
+    }
+
+    public static String doZbs(){
+        String zbs = "vse zbs";
+        return zbs; 
+    }
+}
+
